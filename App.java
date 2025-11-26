05e2644c02b59a2ab959e6169968a0bc936ce5f0
sinap55 <parvanehs@limestone.on.ca>
2025-11-26 13:09:55 -0500

    Created 3 different colour buttons

--- App.java
+++ App.java
@@ -6,7 +6,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 /**
@@ -20,47 +22,54 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
         // Create components to add.
-        VBox contentBox = new VBox();
-        contentBox.setAlignment(Pos.CENTER);
+        VBox gameBox = new VBox();
+        gameBox.setAlignment(Pos.CENTER);
+        HBox aligner = new HBox();
+        aligner.setAlignment(Pos.TOP_CENTER);
 
         Label promptLabel = new Label();
-        promptLabel.setText("Enter your thoughts");
+        promptLabel.setText("SIMON");
 
-        TextField thoughtsBox = new TextField();
-        thoughtsBox.setMaxWidth(150);
-        thoughtsBox.setPromptText("type here");
+        Circle circle1 = new Circle(40); // radius
+
+        Button RedBtn = new Button();
+        RedBtn.setShape(circle1);
+        RedBtn.setMinSize(80, 80); // ensure size fits the circle
+        RedBtn.setMaxSize(80, 80);
+        RedBtn.setAlignment(Pos.TOP_LEFT);
+        RedBtn.setStyle("-fx-background-color: red; -fx-text-fill: red;");
+
+
+        Button greenBtn = new Button();
+        greenBtn.setShape(circle1);
+       greenBtn.setMinSize(80, 80); // ensure size fits the circle
+        greenBtn.setMaxSize(80, 80);
+        greenBtn.setAlignment(Pos.TOP_LEFT);
+        greenBtn.setStyle("-fx-background-color: green; -fx-text-fill: green;");
+
+        Button yellowBtn = new Button();
         
-        TextArea messageBox = new TextArea();
-        messageBox.setEditable(false);
+        Circle circle2 = new Circle(40); // radius
 
-        Button submissionBtn = new Button();
-        submissionBtn.setText("Submit");
-
+        yellowBtn.setShape(circle2);
+        yellowBtn.setMinSize(80, 80); // ensure size fits the circle
+        yellowBtn.setMaxSize(80, 80);
+        yellowBtn.setAlignment(Pos.TOP_RIGHT);
+        yellowBtn.setStyle("-fx-background-color: yellow; -fx-text-fill: yellow;");
         // Set up reactions (aka callbacks).
-        submissionBtn.setOnAction(event -> onSubmitThought(thoughtsBox, messageBox));
+
 
         // Add components to the content box.
-        contentBox.getChildren().add(promptLabel);
-        contentBox.getChildren().add(thoughtsBox);
-        contentBox.getChildren().add(submissionBtn);
-        contentBox.getChildren().add(messageBox);
+        gameBox.getChildren().addAll(aligner);
+        aligner.getChildren().addAll(RedBtn,yellowBtn,greenBtn);
+
 
         // Set up the window and display it.
-        Scene scene = new Scene(contentBox, 300, 200);
+        Scene scene = new Scene(gameBox, 300, 200);
         stage.setScene(scene);
-        stage.setTitle("Amazing App 2000");
+        stage.setTitle("Simon");
         stage.show();
     }
 
-    /**
-     * Handle the submission of a thought.
-     * @param inputBox  The TextField where the user types their thought.
-     * @param outputBox The TextArea where the submitted thoughts are displayed.
-     */
-    void onSubmitThought(TextField inputBox, TextArea outputBox) {
-        String text = inputBox.getText();
-        inputBox.clear();
-        System.out.println("Interesting thought: " + text);
-        outputBox.appendText("Interesting thought: " + text + "\n");
-    }
+
 }
