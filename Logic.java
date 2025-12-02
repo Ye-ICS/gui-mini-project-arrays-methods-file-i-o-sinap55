a3ccd88dbdcb5d298606230d8c5adb773414642e
sinap55 <parvanehs@limestone.on.ca>
2025-12-02 13:10:39 -0500

    basic terminal game

--- Logic.java
+++ Logic.java
@@ -2,31 +2,44 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.Scanner;
 
 public class Logic {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int colorgenerator = ThreadLocalRandom.current().nextInt(1, 4 + 1);
-        System.out.println(colorgenerator);
-        String color = null;
-        if (colorgenerator == 1) {
-            System.out.println("Click Red");
-            color = "red";
-        } else if (colorgenerator == 2) {
-            System.out.println("Click Blue");
-            color = "blue";
-        } else if (colorgenerator == 3) {
-            System.out.println("Click Yellow");
-            color = "yellow";
-        } else if (colorgenerator == 4) {
-            System.out.println("Click Green");
-            color = "green";
+        int[] colorgenerator = new int[5];
+
+        for (int i = 0; i < colorgenerator.length; i++) {
+            colorgenerator[i] = ThreadLocalRandom.current().nextInt(1, 5);
         }
-        System.out.println("What colour should u click");
-        String userInput = sc.nextLine();
 
+        for (int stage = 1; stage <= colorgenerator.length; stage++) {
+            for (int i = 0; i < stage; i++) {
+                if (colorgenerator[i] == 1) {
+                    System.out.println("Click Red");
 
+                } else if (colorgenerator[i] == 2) {
+                    System.out.println("Click Blue");
 
-        if (userInput .equals(color) ) {
-            System.out.println("Correct");
+                } else if (colorgenerator[i] == 3) {
+                    System.out.println("Click Yellow");
+
+                } else if (colorgenerator[i] == 4) {
+                    System.out.println("Click Green");
+
+                }
+            }
+            System.out.println("Take a guess");
+            for (int i = 0; i < stage; i++) {
+                
+                String userInput = sc.nextLine();
+                if (colorgenerator[i] == Integer.parseInt(userInput)) {
+                    System.out.println("Good");
+
+                }
+                else {
+                    System.out.println("Wrong");
+                }
+
+            }
         }
     }
 }
